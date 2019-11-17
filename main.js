d00e03548c05eedcebc26a2d4586e55f3ae1a255
Claire <claire.isabella.ramos@gmail.com>
2019-11-17 16:34:43 -0500

    Added grid for introbox, preliminary text added for introbox

---
+++ main.js
@@ -0,0 +1,13 @@
+// class Pokemon {
+//     constructor(name) {
+//         this.name = name
+//         this.type = []
+//         this.hp = hp
+//         this.attack = attack
+//         this.defense = defense
+//         this.abilities = []
+//     }
+
+//     trainer()
+
+// }
